8744b1acf9b5d685ccd40ad9bdb706acd55bd03a
masum-2003 <billah.naima@gmail.com>
2026-03-08 17:30:06 +0600

    Home page undergo

--- Script/login.js
+++ Script/login.js
@@ -1,1 +1,25 @@
-console.log("Login functioalitity comming")
+console.log("Login functioalitity comming");
+
+document.getElementById("login-btn").addEventListener("click", function(){
+    // get the movile number input
+    const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
+    // get the pin
+    const inputPin = document.getElementById("input-pin");
+    const pin = inputPin.value;
+    console.log(pin);
+    // match pin and mobile number
+    if(contactNumber == "01234567890" && pin == "1234"){
+        // true:::>> alert> homepage
+        alert("login Success")
+        // window.location.replace("/home.html")
+        window.location.assign("/home.html")
+    }
+    else{
+        // true:::>> alert> return
+        alert("Login Failed");
+        return;
+    }
+    
+})
